20c0e620ad3a34aa9d3192a6f3965c52d9136419
yunxu10 <45143770+yunxu10@users.noreply.github.com>
2018-11-18 21:07:57 +0800

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,32 @@
+const Discord = require('discord.js')
+const client = new Discord.Client()
+const prefix = "+";
+
+client.on('ready', () => {
+    // Set bot status to: "Playing with JavaScript"
+    client.user.setActivity("over the guild", {type: "WATCHING"})
+    console.log('I am ready!');
+
+    // Alternatively, you can set the activity to any of the following:
+    // PLAYING, STREAMING, LISTENING, WATCHING
+    // For example:
+    // client.user.setActivity("TV", {type: "WATCHING"})
+})
+
+client.on("message", (message) => {
+  // Exit and stop if it's not there
+  if (!message.content.startsWith(prefix)) return;
+ 
+  if (message.content.startsWith(prefix + "ping")) {
+    message.channel.send(new Date().getTime() - message.createdTimestamp + "`ms` pong~ pong~ pong!");
+  } else
+   if (message.content.startsWith(prefix + "invite")) {
+    message.channel.send("`Rosenheim` server invite link: https://discord.gg/8HSBMYQ");
+  }
+   if (message.content.startsWith(prefix + "belew" )) {
+    message.channel.send("Tama! Belew talaga si Sensei~");
+  }
+ 
+});
+
+client.login(process.env.450bDEK-mWURj03UC1AEQZjPljzP9ixi);
